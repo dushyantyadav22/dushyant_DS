b2b5c8c126e8aae3143a75bf51047925d36b8613
dushyantyadav22 <devrao3212@gmail.com>
2026-02-06 14:41:17 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+int search(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            return -1;
+        }
+    }
+    return -1;
+}
+int bsearch(int arr[], int n, int x)
+{
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (x > arr[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        } 
+        return -1;
+    }   
+}
+int bsearch_f(int arr[],int n,int x)
+{
+    int low=0;
+    int high = n; -1;
+    while(low <= high)
+    {
+        int mid = (low+high) / 2;
+        if( x > arr[mid])
+        {
+            low = mid + 1;
+        }
+        else if(x < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            if(arr[mid]! = arr[mid - 1] || mid==0) 
+            {
+                return mid;
+            }
+            high = mid-1;
+        }
+    }
+    return -1;
+}
+int bsearch_r(int arr [],int n,int x,int low , int high)
+{
+    if(low<high)
+    {
+        return -1;
+    }
+    int mid=(low+high)/2;
+    if(arr[mid]==x)
+    {
+        return mid;
+    }
+    else if(x>arr[mid])
+    {
+        return bsearch_r(arr,n,x,mid+1,high);
+    }
+    else
+    {
+        return bsearch_r(arr,n,x,low,mid-1);
+    }
+}
+int main()
+{
+    int arr[]={10,20,30,40,50,60,70};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    // printf("%d\n",search(arr,n,60));
+    // printf("%d\n",bsearch(arr,n,60));
+    //
+    // printf("%d\n",bsearch_rec(arr,n,60,0,n-1));
+    printf("%d\n",bsearch_f(arr,n,3));
+}
